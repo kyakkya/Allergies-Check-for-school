ccfa8ae1c15af1aab5cf76e2c3fda67859241a03
kyakkya　 <inataaiko123@gmail.com>
2021-04-07 10:57:13 +0000

    Finish editing-users

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,8 @@
 User.create!(name: "Sample User",
              email: "sample@email.com",
              password: "password",
-             password_confirmation: "password")
+             password_confirmation: "password",
+             admin: true)
              
 User.create!(name: "管理職",
              email: "kanri@email.com",
